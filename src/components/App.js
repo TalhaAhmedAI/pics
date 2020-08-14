02f81b0ddfa0d7ef66a8cafba09d760174a6194c
tester <aiforeveryoneai@gmail.com>
2020-08-14 05:10:34 +0500

    Update searchBar.jsx
    Added onFormSubmit method to it

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./searchBar";
+
 function App() {
   return (
     <div className="ui container" style={{ marginTop: "10px" }}>
